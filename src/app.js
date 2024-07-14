1e29dda1f32b5d120f9c18ba697b1dfb62d4674c
CodeAaronM <aaron.martinez.b@gmail.com>
2024-07-14 11:36:10 +0000

    1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateUrl();
+};
+
+const generateUrl = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io", "etc."];
+
+  let Url = "";
+  pronoun.forEach(p => {
+    adj.forEach(a => {
+      noun.forEach(n => {
+        ext.forEach(e => {
+          Url += p + a + n + e + "<br>";
+        });
+      });
+    });
+  });
+
+  return Url;
 };
