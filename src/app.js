c06132342311a6dca5e620ecc6bb19cd4a08c95e
CodeAaronM <aaron.martinez.b@gmail.com>
2024-07-19 19:17:14 +0000

    mejora 0.1

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  document.querySelector("#excuse").innerHTML = generateDomain();
+  document.querySelector("#pronouns").innerHTML = printAll(pronouns);
+  document.querySelector("#adjs").innerHTML = printAll(adjs);
+  document.querySelector("#nouns").innerHTML = printAll(nouns);
+  document.querySelector("#extemcions").innerHTML = printAll(extencions);
+  document.querySelector("#domains").innerHTML = generateDomain();
 };
 
+let pronouns = ["the", "our"];
+let adjs = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+let extencions = [".com", ".net", ".us", ".io", ".etc"];
+let spacing = "<br>";
+
 const generateDomain = () => {
-  let pronouns = ["the", "our"];
-  let adjs = ["great", "big"];
-  let nouns = ["jogger", "racoon"];
-  let extencions = [".com", ".net", ".us", ".io", ".etc"];
-  let spacing = "<br>";
-
   let domain = "";
-
   pronouns.forEach(pronoun => {
     adjs.forEach(adj => {
       nouns.forEach(noun => {
@@ -26,3 +29,10 @@ const generateDomain = () => {
 
   return domain;
 };
+function printAll(pronouns) {
+  let result = "";
+  pronouns.forEach(element => {
+    result += element + "<br>";
+  });
+  return result;
+}
